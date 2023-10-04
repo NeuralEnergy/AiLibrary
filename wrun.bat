67b578c69c7576436cab0f286281cddeccf97da6
Mihai <cristea.mihai@gmail.com>
2023-10-04 08:18:38 +0300

    fix: wrun script

--- wrun.bat
+++ wrun.bat
@@ -1,1 +1,1 @@
-docker run --rm --name local_ne --pull=always -p 5002-5010:5002-5010 --env-file ./.env -v ai_vol:/aid_app/_cache safeweb/ai
+docker run --rm --name NeuralEnergy --pull=always -p 5002-5010:5002-5010 --env-file ./.env -v ai_vol:/aid_app/_cache neuralenergy/ai_library
